b9a461995ff494d9026c44e9ac003fc3c98eb8c0
Ahmed Alghoul <amghoul@gmail.com>
2026-06-12 14:35:17 +0300

    Answering Q2

--- src/Assignment08.py
+++ src/Assignment08.py
@@ -61,12 +61,59 @@ def q1(df: pd.DataFrame, charts_save_location: str)-> None:
     #plt.show()
     log.info(f"The Q1's chart is saved in {os.path.join(charts_save_location,'AQ1.png')} ")
 
+def q2(df: pd.DataFrame, charts_save_location: str)-> None:
+    """Answering Q2
+    Choose any player with 15+ games. Plot raw rating + rolling(5) + expanding avg. 
+    Annotate their highest-rated game
+    """
+    white_games= df['white_id'].value_counts()
+    player_name = white_games[white_games >=15].index[0]
+    player = df[df['white_id']==player_name].sort_values('game_id').reset_index(drop=True)
+    player['rolling_5_rating'] = player['white_rating'].rolling(window=5).mean()
+    player['cum_avg_rating' ] =player['white_rating' ].expanding().mean()
+
+    max_rating = player['white_rating'].max()
+    max_game_index = player['white_rating'].idxmax()
+
+    plt.figure(figsize=(12, 6))
+
+    plt.plot(player.index, player['white_rating'], label='Raw Rating', color='lightgray', alpha=0.9, linestyle='--')
+    plt.plot(player.index, player['rolling_5_rating'], label='5-Game Rolling Average', color='blue', linewidth=2)
+    plt.plot(player.index, player['cum_avg_rating'], label='Cumulative Average', color='orange', linewidth=2)
+
+    # Annotate the highest-rated -game
+    plt.annotate(
+        f'Peak Rating: {max_rating}', 
+        xy=(max_game_index, max_rating),         
+        xytext=(max_game_index + 2, max_rating + 15), 
+        arrowprops=dict(
+            facecolor='black',                    
+            arrowstyle='->',                      #
+            lw=1.5                                
+        ),
+        fontweight='bold',
+        color='red'                               
+    )
+
+    plt.scatter(max_game_index, max_rating, color='red', s=50, zorder=5)
+    # Add chart elements
+    plt.title(f'Rating Progression Trends for Player: {player_name}', fontsize=14, fontweight='bold')
+    plt.xlabel('Number of Games Played')
+    plt.ylabel('Rating')
+    plt.legend(title='Rating Metrics')  # Explains which line is which
+    plt.grid(True, alpha=0.3)
+    plt.savefig(os.path.join(charts_save_location,'AQ2.png'), dpi=150, bbox_inches='tight')
+    log.info(f"The Q2's chart is saved in {os.path.join(charts_save_location,'AQ2.png')} ")
+    #plt.show()
+
+
 def main():
     print("This is for session 8: Visualizations")
     csv_save_location = os.path.join("data","raw")
     charts_save_location = os.path.join("output","charts")
     chess, netflix,temp, temp_monthly = download_load_datasets(csv_save_location)
     q1(chess,charts_save_location)
+    q2(chess,charts_save_location)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
